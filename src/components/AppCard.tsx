5a7d78ffca0d8ca587227b4cbfc7fefcb8b15b5a
YimingIsCOLD <yimingpeh@gmail.com>
2026-01-26 11:41:03 +0800

    refactor(homeview): remove unnecessary apps data extraction and use breakpoints instead of grid container

--- src/components/AppCard.tsx
+++ src/components/AppCard.tsx
@@ -1,48 +1,80 @@
 import { cn, Typography } from '@flow/core';
+import { type Icon as FlowIcon } from '@flow/icons';
 import React from 'react';
-import { Link } from 'react-router';
+import { Link, type LinkProps } from 'react-router';
 
-interface AppCardProps {
-  name: string;
+interface AppCardBaseProps {
+  /**
+   * The icon to display in the card.
+   */
+  icon: FlowIcon;
+  /**
+   * The title to display in the card.
+   */
+  title: string;
+  /**
+   * The description to display in the card.
+   */
   description: string;
-  href: string;
-  icon: React.ComponentType;
-  iconProps?: Record<string, string | number>;
-  variant?: 'featured' | 'default';
-  className?: string;
-  external?: boolean;
+  /**
+   * The direction to display the card.
+   */
+  direction?: 'vertical' | 'horizontal';
 }
 
-const AppCard = ({
-  name,
-  description,
-  icon: Icon,
-  iconProps,
-  variant = 'default',
-  href,
-  external = false,
-  className,
-}: AppCardProps) => {
-  const variantStyles =
-    variant === 'featured'
-      ? 'bg-slate-2 items-center active:bg-slate-5'
-      : 'bg-white-default flex-col active:bg-slate-6';
+interface AppCardAsAnchorProps
+  extends
+    AppCardBaseProps,
+    Omit<React.ComponentPropsWithoutRef<'a'>, 'title' | 'href' | 'children'> {
+  /**
+   * If provided, the card will be rendered as an anchor element.
+   */
+  href: string;
+  to?: never;
+}
 
-  const baseClassName = cn(
-    'gap-lg p-md flex rounded-3xl border cursor-pointer hover:bg-slate-4 focus:bg-slate-4 border-slate-6',
-    variantStyles,
+interface AppCardAsLinkProps
+  extends AppCardBaseProps, Omit<LinkProps, 'title' | 'to' | 'children'> {
+  href?: never;
+  /**
+   * If provided, the card will be rendered as a link element.
+   */
+  to: string;
+}
+
+interface AppCardAsDivProps
+  extends AppCardBaseProps, Omit<React.ComponentPropsWithoutRef<'div'>, 'title' | 'children'> {
+  href?: never;
+  to?: never;
+}
+
+export type AppCardProps = AppCardAsAnchorProps | AppCardAsLinkProps | AppCardAsDivProps;
+
+const AppCard: React.FC<AppCardProps> = ({
+  icon: Icon,
+  title,
+  description,
+  direction = 'vertical',
+  className,
+  ...props
+}) => {
+  const cardClassName = cn(
+    'flex cursor-pointer rounded-3xl border border-slate-6 p-md focus-standard outline-offset-0 transition-[background-color,outline] hover:bg-slate-4 active:bg-slate-5',
+    direction === 'vertical'
+      ? 'flex-col items-start gap-md bg-white-default'
+      : 'flex-row items-center gap-lg bg-slate-2',
     className,
   );
 
-  const content = (
+  const cardContent = (
     <>
-      <div className="w-fit rounded-2xl border border-slate-6 bg-white-default p-lg">
-        <Icon {...iconProps} />
+      <div className="rounded-2xl border border-slate-6 bg-white-default p-lg">
+        <Icon />
       </div>
 
       <div className="flex flex-col gap-xs">
         <Typography variant="label-md-strong" className="text-olive-12">
-          {name}
+          {title}
         </Typography>
         <Typography variant="body-sm" className="line-clamp-2 text-olive-11">
           {description}
@@ -51,18 +83,26 @@ const AppCard = ({
     </>
   );
 
-  if (external) {
+  if ('href' in props && props.href !== undefined) {
     return (
-      <a href={href} className={baseClassName} target="_blank" rel="noopener noreferrer">
-        {content}
+      <a target="_blank" rel="noopener noreferrer" className={cardClassName} {...props}>
+        {cardContent}
       </a>
     );
   }
 
+  if ('to' in props && props.to !== undefined) {
+    return (
+      <Link className={cardClassName} {...props}>
+        {cardContent}
+      </Link>
+    );
+  }
+
   return (
-    <Link to={href} className={baseClassName}>
-      {content}
-    </Link>
+    <div className={cardClassName} {...props}>
+      {cardContent}
+    </div>
   );
 };
 
--- src/containers/HomeView.tsx
+++ src/containers/HomeView.tsx
@@ -1,79 +1,91 @@
 import { Typography } from '@flow/core';
-import { Folder, Heart } from '@flow/icons';
+import { Heart } from '@flow/icons';
 import React from 'react';
 
 import AppCard from '~/components/AppCard';
-import { SidebarTrigger } from '~/components/Sidebar';
-import { Book } from '~/icons';
-
-interface AppCardData {
-  id: number;
-  name: string;
-  description: string;
-  icon: React.ComponentType;
-  href: string;
-  iconProps?: Record<string, string | number>;
-  featured?: boolean;
-  external?: boolean;
-}
-
-interface Section {
-  title: string;
-  apps: AppCardData[];
-}
-
-const getGreeting = () => {
-  const hour = new Date().getHours();
-
-  if (hour >= 5 && hour < 12) return 'Good morning';
-  else if (hour >= 12 && hour < 18) return 'Good afternoon';
-  else return 'Good evening';
-};
-
-const AppSection = ({ title, apps, variant }: Section & { variant?: 'featured' | 'default' }) => (
-  <div className="gap-md flex flex-col">
-    <Typography variant="label-lg-strong">{title}</Typography>
-    <div
-      className={
-        variant === 'featured' ? 'flex flex-col' : 'gap-sm grid grid-cols-6 lg:grid-cols-12'
-      }
-    >
-      {apps.map(({ id, ...props }) => (
-        <AppCard
-          key={id}
-          className={variant === 'featured' ? '' : 'col-span-3 lg:col-span-4'}
-          variant={variant}
-          {...props}
-        />
-      ))}
-    </div>
-  </div>
-);
+import { getDayPeriod } from '~/helpers/dateTime';
 
 const HomeView: React.FC = () => {
-  const featuredApps = apps.filter((app) => app.featured);
-  const greeting = getGreeting();
+  const dayPeriod = getDayPeriod();
 
   return (
-    <div className="p-md relative w-full flex-1">
-      <SidebarTrigger className="lg:hidden" />
+    <div className="mx-auto flex max-w-3xl flex-col gap-14 px-md pt-3xl pb-3xl sm:pt-20">
+      <Typography variant="title-lg" className="p-sm text-center">
+        Good {dayPeriod}
+      </Typography>
 
-      <div className="grid grid-cols-6 lg:grid-cols-12">
-        <div className="col-span-6 lg:col-start-3 lg:col-end-11">
-          <Typography variant="title-lg" className="mt-3 text-center lg:mt-20">
-            {greeting}
-          </Typography>
+      <div className="flex flex-col gap-3xl">
+        <div className="flex flex-col gap-md">
+          <Typography variant="label-lg-strong">Featured</Typography>
 
-          <div className="gap-2xl lg:gap-3xl mt-14 flex flex-col">
-            {/* Featured */}
-            {featuredApps.length > 0 && (
-              <AppSection title="Featured" apps={featuredApps} variant="featured" />
-            )}
+          <AppCard
+            icon={Heart}
+            title="App name"
+            description="App description. Max two lines, for example this is a line where it will truncate if it goes longer."
+            direction="horizontal"
+            href="https://transform.gov.sg"
+          />
+        </div>
 
-            {/* Classroom and Parent Sections */}
-            {sections.map(({ title, apps }) =>
-              apps.length > 0 ? <AppSection key={title} title={title} apps={apps} /> : null,
-            )}
+        <div className="flex flex-col gap-md">
+          <Typography variant="label-lg-strong">Classroom and Student</Typography>
+
+          <div className="grid grid-cols-2 gap-md md:grid-cols-3">
+            <AppCard
+              icon={Heart}
+              title="App name"
+              description="App description. Max two lines, for example this is a line where it will truncate if it goes longer."
+              href="https://transform.gov.sg"
+            />
+            <AppCard
+              icon={Heart}
+              title="App name"
+              description="App description. Max two lines, for example this is a line where it will truncate if it goes longer."
+              href="https://transform.gov.sg"
+            />
+            <AppCard
+              icon={Heart}
+              title="App name"
+              description="App description. Max two lines, for example this is a line where it will truncate if it goes longer."
+              href="https://transform.gov.sg"
+            />
+            <AppCard
+              icon={Heart}
+              title="App name"
+              description="App description. Max two lines, for example this is a line where it will truncate if it goes longer."
+              href="https://transform.gov.sg"
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-md">
+          <Typography variant="label-lg-strong">Parent and Communication</Typography>
+
+          <div className="grid grid-cols-2 gap-md md:grid-cols-3">
+            <AppCard
+              icon={Heart}
+              title="App name"
+              description="App description. Max two lines, for example this is a line where it will truncate if it goes longer."
+              href="https://transform.gov.sg"
+            />
+            <AppCard
+              icon={Heart}
+              title="App name"
+              description="App description. Max two lines, for example this is a line where it will truncate if it goes longer."
+              href="https://transform.gov.sg"
+            />
+            <AppCard
+              icon={Heart}
+              title="App name"
+              description="App description. Max two lines, for example this is a line where it will truncate if it goes longer."
+              href="https://transform.gov.sg"
+            />
+            <AppCard
+              icon={Heart}
+              title="App name"
+              description="App description. Max two lines, for example this is a line where it will truncate if it goes longer."
+              href="https://transform.gov.sg"
+            />
           </div>
         </div>
       </div>
@@ -82,62 +94,3 @@ const HomeView: React.FC = () => {
 };
 
 export default HomeView;
-
-const apps: AppCardData[] = [
-  {
-    id: 1,
-    name: 'App name 1',
-    description: 'App description. Max two lines.',
-    icon: Heart,
-    iconProps: { stroke: 'var(--color-crimson-10)', fill: 'var(--color-crimson-10)', size: 32 },
-    href: '/app/1',
-    featured: true,
-    external: true,
-  },
-  {
-    id: 2,
-    name: 'App name 2',
-    description:
-      'App description. Max two lines, for example this is a line where it will truncate if it goes longer.',
-    icon: Folder,
-    iconProps: { stroke: 'var(--color-blue-9)', fill: 'var(--color-blue-9)', size: 32 },
-    href: '/app/2',
-  },
-  {
-    id: 3,
-    name: 'App name 3',
-    description:
-      'App description. Max two lines, for example this is a line where it will truncate if it goes longer.',
-    icon: Folder,
-    iconProps: { stroke: 'var(--color-blue-9)', fill: 'var(--color-blue-9)', size: 32 },
-    href: '/app/3',
-  },
-  {
-    id: 4,
-    name: 'App name 4',
-    description:
-      'App description. Max two lines, for example this is a line where it will truncate if it goes longer.',
-    icon: Book,
-    href: '/app/4',
-  },
-  {
-    id: 5,
-    name: 'App name 5',
-    description:
-      'App description. Max two lines, for example this is a line where it will truncate if it goes longer.',
-    icon: Heart,
-    iconProps: { stroke: 'var(--color-crimson-10)', fill: 'var(--color-crimson-10)', size: 32 },
-    href: '/app/5',
-  },
-];
-
-const sections: Section[] = [
-  {
-    title: 'Classroom and Student',
-    apps: apps,
-  },
-  {
-    title: 'Parent and Communication',
-    apps: apps.slice(2),
-  },
-];
