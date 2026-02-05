a61e09e2f09b1923641f5753560591e44e33749d
kellylim <kellylim.06@gmail.com>
2026-02-05 18:28:27 +0800

    feat: add and set cookie header

--- server/internal/routes/otp.go
+++ server/internal/routes/otp.go
@@ -1,13 +1,34 @@
 package routes
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func RequestOTP(w http.ResponseWriter, r *http.Request) {
+	cookie := http.Cookie{
+		Name:     "session_id",
+		Value:    "123456",
+		Path:     "/",
+		Secure:   true,
+		HttpOnly: true,
+		SameSite: http.SameSiteLaxMode,
+	}
+	http.SetCookie(w, &cookie)
+
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("RequestOTP"))
+
 }
 
 func VerifyOTP(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("VerifyOTP"))
+
+	cookie, err := r.Cookie("session_id")
+	if err != nil {
+		fmt.Println("There is an error")
+	} else {
+		fmt.Println(cookie.Value)
+	}
 }
