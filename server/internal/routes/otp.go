a52362ae0d0d7644ed16539fbf0cd1024882d343
kellylim <kellylim.06@gmail.com>
2026-02-05 19:00:31 +0800

    feat: echo request payload and return session cookie

--- server/internal/routes/otp.go
+++ server/internal/routes/otp.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -16,19 +17,28 @@ func RequestOTP(w http.ResponseWriter, r *http.Request) {
 	}
 	http.SetCookie(w, &cookie)
 
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("RequestOTP"))
+	bodyBytes, err := io.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println("There is an error")
+	} else {
+		fmt.Println("No error")
+	}
 
+	bodyString := string(bodyBytes)
+	fmt.Println("Body String:", bodyString)
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(bodyBytes)
 }
 
 func VerifyOTP(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("VerifyOTP"))
-
 	cookie, err := r.Cookie("session_id")
 	if err != nil {
 		fmt.Println("There is an error")
 	} else {
 		fmt.Println(cookie.Value)
 	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(cookie.Value))
 }
