76539ee9dc43180b279b84269347c170cba04c62
kellylim <kellylim.06@gmail.com>
2026-02-06 18:04:57 +0800

    feat: generate and set secure session_id cookie for otp request

--- server/internal/routes/otp.go
+++ server/internal/routes/otp.go
@@ -1,21 +1,49 @@
 package routes
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
 	"io"
 	"net/http"
 )
 
+var store = make(map[string]map[string]string)
+
 func RequestOTP(w http.ResponseWriter, r *http.Request) {
-	cookie := http.Cookie{
-		Name:     "session_id",
-		Value:    "123456",
-		Path:     "/",
-		Secure:   true,
-		HttpOnly: true,
-		SameSite: http.SameSiteLaxMode,
+	c, err := r.Cookie("session_id")
+	store["session_id"] = map[string]string{"otp_flow_id": "123"}
+	fmt.Println(store["session_id"])
+
+	if err != nil {
+		id := make([]byte, 32)
+		if _, err := rand.Read(id); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		sessionID := base64.RawURLEncoding.EncodeToString(id)
+
+		cookie := http.Cookie{
+			Name:     "session_id",
+			Value:    sessionID,
+			Path:     "/",
+			Secure:   true,
+			HttpOnly: true,
+			SameSite: http.SameSiteLaxMode,
+		}
+		http.SetCookie(w, &cookie)
+	} else {
+		cookie := http.Cookie{
+			Name:     "session_id",
+			Value:    c.Value,
+			Path:     "/",
+			Secure:   true,
+			HttpOnly: true,
+			SameSite: http.SameSiteLaxMode,
+		}
+		http.SetCookie(w, &cookie)
 	}
-	http.SetCookie(w, &cookie)
 
 	bodyBytes, err := io.ReadAll(r.Body)
 	if err != nil {
