0cfc1e203613a4162c3ff0b5c5c15eb6a607f25b
Yi Ning SOONG <yiningsoong@gmail.com>
2026-02-09 14:13:52 +0800

    chore(ci): remove pre-commit lint

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,8 +2,4 @@
 export default {
   '*.{js,jsx,ts,tsx,md,html,css,json,yaml}': 'prettier --write',
   '*.{js,jsx,ts,tsx}': 'eslint --fix',
-  '*.go': (files) => {
-    const dirs = new Set(files.map((file) => file.substring(0, file.lastIndexOf('/'))));
-    return Array.from(dirs).map((dir) => `golangci-lint run --fix ${dir}`);
-  },
 };
