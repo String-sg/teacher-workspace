fd60980c60ba55bedf9946fe25dbba92f5542831
kellylimmm <54774084+kellylimmm@users.noreply.github.com>
2026-06-22 11:39:09 +0800

    chore: scaffold Go HTTP server with slog and graceful shutdown (#22)

---
+++ server/cmd/tw/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/String-sg/teacher-workspace/server/internal/handler"
+)
+
+const (
+	addr            = ":3000"
+	shutdownTimeout = 30 * time.Second
+)
+
+func main() {
+	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout, nil)))
+
+	srv := &http.Server{
+		Addr:    addr,
+		Handler: handler.NewMux(),
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	go func() {
+		slog.Info("listening", "addr", addr)
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			slog.Error("listen failed", "err", err)
+			os.Exit(1)
+		}
+	}()
+
+	<-ctx.Done()
+	stop()
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), shutdownTimeout)
+	defer cancel()
+
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		slog.Error("shutdown failed", "err", err)
+		os.Exit(1)
+	}
+}
